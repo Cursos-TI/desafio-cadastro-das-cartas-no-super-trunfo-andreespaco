3424c66218c4b558c8475a692c1906d46902ca69
andreespaco <150025491+andreespaco@users.noreply.github.com>
2025-02-11 19:55:37 +0000

    comecei a comparar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,9 +12,13 @@
 int main() {
   
  int cdc, populacao, pontos;
+ int cdc2, populacao2, pontos2;
  float area, pib, densidade, pib2;
+ float area2, pib02, densidade2, pib3;
  char nome[50];
+ char nome2[50];
  
+ printf("Insira os dados da primeira carta\n");
 printf("Digite o codigo da cidade\n");
 scanf("%d" , &cdc);
 
@@ -33,9 +37,35 @@ scanf("%d" , &pontos);
 printf("DIgite o pib total:\n");
 scanf("%f", &pib);
 
-densidade = populacao / area;
-pib2 = pib / populacao;
+//Agora vamos inserir os dados da segunda carta
+//Agora as variaveis tem o mesmo nome + o numero 2
+//Exeto pela variavel "pib" ela sea adicionada 02 "pib02" e o pib per capita "pib3"
+printf("Agora insira os dados da segunda carta\n");
+printf("Digite o codigo da cidade\n");
+scanf("%d" , &cdc2);
 
+printf("Digite o nome da cidade\n");
+scanf("%s" , &nome2);
+
+printf("Digite o numero da populacao\n");
+scanf("%d" , &populacao2);
+
+printf("Digite a area da cidade\n");
+scanf("%f" , &area2);
+
+printf("Didite o numero de pontos turisticos\n");
+scanf("%d" , &pontos2);
+
+printf("DIgite o pib total:\n");
+scanf("%f", &pib2);
+
+
+densidade = populacao / area;
+densidade2 = populacao2 / area2;
+pib2 = pib / populacao;
+pib3 = pib02 / pontos2;
+
+printf("Eses sao os dados da primeira carta:\n");
 printf("Codigo da cidade %d\n", cdc);
 printf("Nome da cidade %s\n",nome);
 printf("Populacao %d\n", populacao);
@@ -43,7 +73,26 @@ printf("Area %.3f\n", area);
 printf("Pib %.3f\n", pib);
 printf("Pontos turisticos %d \n", pontos);
 printf("Densidade populacional %.3f \n",densidade);
-printf("Pib per capita: %.3f", pib2);
+printf("Pib per capita: %.3f \n", pib2);
+
+printf("Eses sao os dados da segunda carta:\n");
+printf("Codigo da cidade %d\n", cdc2);
+printf("Nome da cidade %s\n",nome2);
+printf("Populacao %d\n", populacao2);
+printf("Area %.3f\n", area2);
+printf("Pib %.3f\n", pib2);
+printf("Pontos turisticos %d \n", pontos2);
+printf("Densidade populacional %.3f \n",densidade2);
+printf("Pib per capita: %.3f \n", pib3);
+
+printf("Agora as cartas serao comparadas\n");
+if (populacao > populacao2) {
+    printf("A populacao da primeira cidade venceu");
+}else {
+    printf("A populacao da segunda cidade venceu");
+}
+if (area)
+
 
 return 0;
 
